b3dfab260ea277b5c5773f728b2eba7425cc2b9d
vincilisi <lisitano95@gmail.com>
2025-07-16 05:53:45 +0200

    user7

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -2,29 +2,77 @@
 
 namespace App\Jobs;
 
+use App\Models\Image;
+use Google\Cloud\Vision\V1\Client\ImageAnnotatorClient;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Spatie\Image\Enums\AlignPosition;
+use Spatie\Image\Enums\Fit;
+use Spatie\Image\Image as SpatieImage;
+
 
 class RemoveFaces implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     */
-    public function __construct()
+    private $article_image_id;
+    
+    public function __construct($article_image_id)
     {
-        //
+        $this->article_image_id = $article_image_id;
     }
 
-    /**
-     * Execute the job.
-     */
+    
     public function handle(): void
     {
-        //
+        $i = Image::find($this->article_image_id);
+        if(!$i)
+        {
+            return;
+        }
+
+        $srcPath = storage_path('app/public/' . $i->path);
+        $image = file_get_contents($srcPath);
+
+        putenv('GOOGLE_APPLICATION_CREDENZIALS=' . base_path('google_credential.json'));
+
+        $imageAnnotator = new ImageAnnotatorClient();
+        $response = $imageAnnotator->faceDetection($image);
+        $faces = $response->getFaceAnnotations();
+
+        foreach($faces as $face)
+        {
+            $vertices =$face->getBoundingPoly()->getVertices();
+
+            $bounds = [];
+
+            foreach($vertices as $vertex)
+            {
+                $bounds[] = [$vertex->getX(), $vertex->getY()];
+            }
+            
+            $w = $bounds[2][0] - $bounds[0][0];
+            $h = $bounds[2][1] - $bounds[0][1];
+            $image = Image::load($srcPath);
+            $image = SpatieImage::load($srcPath);
+
+            $image->watermark
+            (
+                base_path('resoureces/img/smile.png'),
+                AlignPosition::TopLeft,
+                paddingX: $bounds[0][0],
+                paddingY: $bounds[0][1],
+                width: $w,
+                height: $h,
+                fit: Fit::Stretch
+            );
+
+            $image->save($srcPath);
+        }
+
+        $imageAnnotator->close();
     }
 }
--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,9 +4,11 @@ namespace App\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
 use Livewire\Attributes\Validate;   
 use Livewire\Component;
 use Livewire\WithFileUploads;
@@ -50,7 +52,19 @@ class CreateArticleForm extends Component
             dispatch(new ResizeImage($newImage->path, 300, 300));
             dispatch(new GoogleVisionSafeSearch($newImage->id));
             dispatch(new GoogleVisionLabelImage($newImage->id));
+
+
+            RemoveFaces::withChain(
+                [
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ]
+            )->dispatch($newImage->id);
         }
+
+        File::deleteDirectory(storage_path('/app/livewire-tmp'));
+
         session()->flash('success', 'Articolo creato correttamente');
         $this->cleanForm();
     }
